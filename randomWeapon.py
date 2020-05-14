ecc57ff89b83b1241b7114fe4406fa0d045bca19
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-05-14 01:40:00 -0500

    turned some variables into proper dictionaries

--- randomWeapon.py
+++ randomWeapon.py
@@ -1,15 +1,25 @@
 import random
 weapons = ["mastiff", "kraber", "lstar", "devotion", "r99", "longbow", "peacekeeper", "hemlok", "prowler", "r301", "g7", "wingman",
            "spitfire", "havoc", "sentinel", "eva8", "flatline", "chargerifle", "re45", "tripletake", "alternator", "p2020", "mozambique"]
-barrel_stabs = ["none", 1, 2, 3, 4]
-light_mags = [1, 2, 3]
-heavy_mags = [1, 2, 3]
-sniper_mags = [1, 2, 3]
-hop_ups = ["doubletap", "choke", "hammerpoint", "selectfire", "skullpiercer"]
-stand_stocks = [1, 2, 3]
-snipe_stocks = [1, 2, 3]
-stocks = [stand_stocks, snipe_stocks]
-mags = [light_mags, heavy_mags, sniper_mags]
-bolts = ["none", 1, 2, 3]
+barrel_stabs = [0, 1, 2, 3, 4]
+#hop_ups = [{"ev8_g7": [0, "doubletap"]}, {"pk_tt": [0, "choke"]}, {"p20_moz": [0, "hammerpoint"]}, {"prow_hav": [0, "selectfire"]}, {"lb_wing": [0, "skullpiercer"]}]
+hop_ups = [[0, "doubletap"], [0, "choke"], [
+    0, "hammerpoint"], [0, "selectfire"], [0, "skullpiercer"]]
+stocks = {"stand_stock": [0, 1, 2, 3], "snipe_stock": [0, 1, 2, 3]}
+mags = [{"light_mag": [0, 1, 2, 3]}, {
+    "heavy_mag": [0, 1, 2, 3]}, {"sniper_mag": [0, 1, 2, 3]}]
+sg_bolts = [0, 1, 2, 3]
+optics = [0, 1, 2, 3, 4, 5]
 rand_weapon = random.choice(weapons)
-print(rand_weapon)
+# print(rand_weapon)
+print(random.choice(stocks["snipe_stock"]))
+
+
+def mastiff():
+    bolt = random.choice(sg_bolts)
+    optic = random.choice(optics)
+    return (f"Mastiff: {bolt}, {optic}")
+
+
+mastiff()
+print(mastiff())
