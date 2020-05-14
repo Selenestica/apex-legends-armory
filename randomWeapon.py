c1e14c015c141d87df3b1505fc173fc608e2232f
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-05-14 00:51:33 -0500

    first commit for apex legends random weapon and attachment generator

---
+++ randomWeapon.py
@@ -0,0 +1,5 @@
+import random
+weapons = ["mastiff", "kraber", "lstar", "devotion", "r99", "longbow", "peacekeeper", "hemlok", "prowler", "r301", "g7", "wingman",
+           "spitfire", "havoc", "sentinel", "eva8", "flatline", "chargerifle", "re45", "tripletake", "alternator", "p2020", "mozambique"]
+rand_weapon = random.choice(weapons)
+print(rand_weapon)
