e94af130e70d46b71b9ba7e26c7ecae5e7053fd4
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-05-14 00:52:12 -0500

    first commit for apex legends random weapon and attachment generator

--- randomWeapon.py
+++ randomWeapon.py
@@ -1,5 +1,15 @@
 import random
 weapons = ["mastiff", "kraber", "lstar", "devotion", "r99", "longbow", "peacekeeper", "hemlok", "prowler", "r301", "g7", "wingman",
            "spitfire", "havoc", "sentinel", "eva8", "flatline", "chargerifle", "re45", "tripletake", "alternator", "p2020", "mozambique"]
+barrel_stabs = ["none", 1, 2, 3, 4]
+light_mags = [1, 2, 3]
+heavy_mags = [1, 2, 3]
+sniper_mags = [1, 2, 3]
+hop_ups = ["doubletap", "choke", "hammerpoint", "selectfire", "skullpiercer"]
+stand_stocks = [1, 2, 3]
+snipe_stocks = [1, 2, 3]
+stocks = [stand_stocks, snipe_stocks]
+mags = [light_mags, heavy_mags, sniper_mags]
+bolts = ["none", 1, 2, 3]
 rand_weapon = random.choice(weapons)
 print(rand_weapon)
