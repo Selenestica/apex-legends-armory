fea568774bb3503fa206bfbbb2b0c6a7fa0f4abd
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-10-09 11:55:54 -0500

    more testing

--- test_req.py
+++ test_req.py
@@ -5,7 +5,7 @@ url = "https://therundown-therundown-v1.p.rapidapi.com/sports/1/dates"
 
 headers = {
     'x-rapidapi-host': "therundown-therundown-v1.p.rapidapi.com",
-    'x-rapidapi-key': "{our-api-key}"
+    'x-rapidapi-key': ""
 }
 
 response = requests.request("GET", url, headers=headers)
@@ -13,6 +13,7 @@ response = requests.request("GET", url, headers=headers)
 dates = response.text
 dates = json.loads(dates)
 dates_list = dates["dates"]
+event_data = {}
 events_list = []
 for date in dates_list:
     event_url = "https://therundown-therundown-v1.p.rapidapi.com/sports/1/events/" + \
@@ -24,5 +25,5 @@ for date in dates_list:
 
 print(type(events_list[0]))
 print(len(events_list))
-stuff = json.loads(events_list)
-print(stuff)
+event_data["data"] = events_list
+print(event_data)
