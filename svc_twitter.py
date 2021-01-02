421b2fe636abf92bf0f13bc41123a4b5dd96c4ed
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2021-01-02 10:37:19 -0600

    made an obj

--- svc_twitter.py
+++ svc_twitter.py
@@ -1,5 +1,22 @@
 import twint  # configuration
 config = twint.Config()
 config.Search = "bitcoin"
-config.Limit = 10  # running search
+config.Limit = 2  # running search
+config.Hide_output = True
+config.Store_object = True
 twint.run.Search(config)
+
+res = twint.output.tweets_list
+tweet_list = []
+limiter = 0
+for tweet in res:
+    limiter += 1
+    if limiter == 6:
+        break
+    else:
+        tweet_obj = {}
+        tweet_obj["username"] = tweet.username
+        tweet_obj["tweet"] = tweet.tweet
+        tweet_obj["date"] = tweet.datetime
+        tweet_list.append(tweet_obj)
+print(tweet_list)
