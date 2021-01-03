672acf47fb8494aaa819a3fdf6c7a7ecfab02bed
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2021-01-02 20:10:55 -0600

    complete for devbot

--- svc_twitter.py
+++ svc_twitter.py
@@ -1,22 +1,28 @@
 import twint  # configuration
+term = "bitcoin"
 config = twint.Config()
-config.Search = "bitcoin"
+config.Search = term
 config.Limit = 2  # running search
-config.Hide_output = True
-config.Store_object = True
-twint.run.Search(config)
-
-res = twint.output.tweets_list
-tweet_list = []
-limiter = 0
-for tweet in res:
-    limiter += 1
-    if limiter == 6:
-        break
+try:
+    config.Hide_output = True
+    config.Store_object = True
+    twint.run.Search(config)
+    res = twint.output.tweets_list
+    if len(res) > 0:
+        limiter = 0
+        body_text = "Tweets:\n"
+        for tweet in res:
+            print(tweet)
+            limiter += 1
+            if limiter == 6:
+                break
+            else:
+                body_text += "Username: " + tweet.username
+                body_text += "\nDate: " + tweet.datetime
+                body_text += "\nTweet: " + tweet.tweet + "\n\n"
     else:
-        tweet_obj = {}
-        tweet_obj["username"] = tweet.username
-        tweet_obj["tweet"] = tweet.tweet
-        tweet_obj["date"] = tweet.datetime
-        tweet_list.append(tweet_obj)
-print(tweet_list)
+        body_text = "We didn't find any tweets about " + term + "."
+except Exception as e:
+    print(e)
+    body_text = "Error: " + e
+print(body_text)
